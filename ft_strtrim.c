e7d5efaf970b557c9f26ec053892777ee99342e1
hboutale <boutaleb.hm@gmail.com>
2024-11-01 13:02:21 +0100

    libft strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboutale <hboutale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 12:34:52 by hboutale          #+#    #+#             */
+/*   Updated: 2024/11/01 13:01:08 by hboutale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	has(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static size_t	start_index(char const *s1, char const *set)
+{
+	size_t	result;
+
+	result = 0;
+	while (s1[result] && has(s1[result], set))
+		result++;
+	return (result);
+}
+static size_t	end_index(char const *s1, char const *set)
+{
+	size_t	result;
+
+	result = ft_strlen((char *)s1) - 1;
+	while (result >= 0 && has(s1[result], set))
+		result--;
+	return (result + 1);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*result;
+
+	start = start_index(s1, set);
+	end = end_index(s1, set);
+	printf("start: %zu end: %zu\n", start, end);
+	i = 0;
+	if (start >= end)
+		return (ft_strdup(""));
+	result = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!result)
+		return (NULL);
+	ft_memcpy(result, (char *)(s1 + start), (end - start));
+	result[end - start] = '\0';
+	return (result);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hboutale <hboutale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:01:42 by hboutale          #+#    #+#             */
-/*   Updated: 2024/11/01 10:39:16 by hboutale         ###   ########.fr       */
+/*   Updated: 2024/11/01 12:56:12 by hboutale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,6 @@ char	ft_toupper(int c);
 void	*ft_calloc(size_t nelem, size_t elsize);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strdup(char *src);
 #endif
